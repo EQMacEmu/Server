71bc305fba56d00d77f2f0a8aa6ca4dbf76fa9be
regneq <rregenn@gmail.com>
2023-07-24 21:16:00 -0700

    added data for loginserversetting

--- loginserver/login_util/tblloginserversettings.sql
+++ loginserver/login_util/tblloginserversettings.sql
@@ -13,3 +13,5 @@ ENGINE=InnoDB
 INSERT INTO `tblloginserversettings` VALUES ('allow_PCT', 'TRUE', 'options', 'allow PC ticketed client', 'TRUE');
 INSERT INTO `tblloginserversettings` VALUES ('allow_OSX', 'TRUE', 'options', 'allow OSX client', 'TRUE');
 INSERT INTO `tblloginserversettings` VALUES ('allow_PC', 'TRUE', 'options', 'allow PC client', 'TRUE');
+INSERT INTO `tblloginserversettings` VALUES ('pop_count', '0', 'options', '0 to only display UP or DOWN or 1 to show population count in server select.', '0');
+INSERT INTO `tblloginserversettings` VALUES ('ticker', 'Welcome to EQMacEmu', 'options', 'Sets the welcome message in server select.', 'Welcome to EQMacEmu');
