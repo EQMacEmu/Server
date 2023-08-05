d8f466c55575f5b9ab70d736fa6892ad6d8ada3c
Lead Generalist MTF Engineer <espause0703@gmail.com>
2023-08-05 18:08:34 -0400

    Fix SQL

--- utils/sql/git/required/2023_08_02_expansions_gating.sql
+++ utils/sql/git/required/2023_08_02_expansions_gating.sql
@@ -8,14 +8,14 @@ ALTER TABLE `lootdrop_entries` ADD COLUMN `expansion` int(11) NOT NULL DEFAULT 0
 ALTER TABLE `ground_spawns` ADD COLUMN `expansion` int(11) NOT NULL DEFAULT 0 AFTER `respawn_timer`;
 REPLACE INTO command_settings`(`command`, `access`) VALUES ('setnpcexpansion', 100);
 
-ALTER TABLE `items` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0
-ALTER TABLE `merchantlist` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0
-ALTER TABLE `spawn2` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0
-ALTER TABLE `spawnentry` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0
-ALTER TABLE `object` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0
-ALTER TABLE `doors` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0
-ALTER TABLE `lootdrop_entries` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0
-ALTER TABLE `ground_spawns` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0
+ALTER TABLE `items` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0;
+ALTER TABLE `merchantlist` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0;
+ALTER TABLE `spawn2` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0;
+ALTER TABLE `spawnentry` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0;
+ALTER TABLE `object` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0;
+ALTER TABLE `doors` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0;
+ALTER TABLE `lootdrop_entries` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0;
+ALTER TABLE `ground_spawns` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0;
 
 ALTER TABLE `items` ADD COLUMN `max_expansion` float NOT NULL DEFAULT 0 AFTER `min_expansion`;
 ALTER TABLE `merchantlist` ADD COLUMN `max_expansion` float NOT NULL DEFAULT 0 AFTER `min_expansion`;
