c4aef90c998005aa31e2083a62805cbb3935fa66
Lead Generalist MTF Engineer <espause0703@gmail.com>
2023-08-08 17:13:47 -0400

    Fix typo in 2023_08_02_expansions_gating.sql

--- utils/sql/git/required/2023_08_02_expansions_gating.sql
+++ utils/sql/git/required/2023_08_02_expansions_gating.sql
@@ -6,7 +6,7 @@ ALTER TABLE `object` ADD COLUMN `expansion` int(11) NOT NULL DEFAULT 0 AFTER `in
 ALTER TABLE `doors` ADD COLUMN `expansion` int(11) NOT NULL DEFAULT 0 AFTER `can_open`;
 ALTER TABLE `lootdrop_entries` ADD COLUMN `expansion` int(11) NOT NULL DEFAULT 0 AFTER `disabled_chance`;
 ALTER TABLE `ground_spawns` ADD COLUMN `expansion` int(11) NOT NULL DEFAULT 0 AFTER `respawn_timer`;
-REPLACE INTO command_settings`(`command`, `access`) VALUES ('setnpcexpansion', 100);
+REPLACE INTO `command_settings`(`command`, `access`) VALUES ('setnpcexpansion', 100);
 
 ALTER TABLE `items` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0;
 ALTER TABLE `merchantlist` CHANGE COLUMN `expansion` `min_expansion` float NOT NULL DEFAULT 0;
