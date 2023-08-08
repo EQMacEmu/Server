8f15a69a009a2d4ee7f519f13ac36fd957a1929c
Lead Generalist MTF Engineer <espause0703@gmail.com>
2023-08-08 12:05:01 -0400

    Adjust current expansion to Torven's specifications

--- common/ruletypes.h
+++ common/ruletypes.h
@@ -122,7 +122,7 @@ RULE_INT(World, StreamDataRate, 50) // Sets the datarate for EQOldStream. Defaul
 RULE_INT(World, WhoListLimit, 20) //The max players returned in /who all.
 RULE_INT(World, MuleToonLimit, 8) // The number of characters a mule account can create/access.
 RULE_BOOL( World, DontBootDynamics, false) // If true, dynamic zones will not boot when a player tries to enter them.
-RULE_REAL( World, CurrentExpansion, 15.0)
+RULE_REAL( World, CurrentExpansion, 6.0)
 RULE_CATEGORY_END()
 
 RULE_CATEGORY( Zone )
