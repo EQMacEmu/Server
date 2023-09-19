8335d745cd5f6f24e2c596ade091b44c0e7544ad
Lead Generalist MTF Engineer <espause0703@gmail.com>
2023-09-18 22:22:48 -0400

    Attempt #2 to fix iplookup

--- world/clientlist.cpp
+++ world/clientlist.cpp
@@ -459,18 +459,17 @@ void ClientList::SendCLEList(const int16& admin, const char* to, WorldTCPConnect
 			if (addnewline) {
 				fmt::format_to(out, newline);
 			}
-			if (strlen(cle->AccountName()) > 0 && strlen(inet_ntoa(in)) > 0)
+			if (cle->AccountID() && cle->AccountName() && cle->AccountName()[0] != 0 && inet_ntoa(in) && inet_ntoa(in)[0] != 0 && strlen(inet_ntoa(in)) > 0)
 			{
 				fmt::format_to(out, "ID: {}  Acc# {}  AccName: {}  IP: {}", cle->GetID(), cle->AccountID(), cle->AccountName(), inet_ntoa(in));
 				fmt::format_to(out, "{}  Stale: {}  Online: {}  Admin: {}", newline, cle->GetStaleCounter(), cle->Online(), cle->Admin());
-				if (cle->LSID() && strlen(cle->LSName()) > 0)
+				if (cle && cle->LSID() && cle->LSName() && cle->LSName()[0] != 0)
 					fmt::format_to(out, "{}  LSID: {}  LSName: {}  WorldAdmin: {}", newline, cle->LSID(), cle->LSName(), cle->WorldAdmin());
-				if (cle->CharID() && strlen(cle->name()) > 0 && strlen(database.GetZoneName(cle->zone())) > 0)
+				if (cle && cle->CharID() && cle->name() && cle->name()[0] != 0 && database.GetZoneName(cle->zone()) && database.GetZoneName(cle->zone())[0] != 0)
 					fmt:format_to(out, "{}  CharID: {}  CharName: {}  Zone: {} ( {} )", newline, cle->CharID(), cle->name(), database.GetZoneName(cle->zone()), cle->zone());
 			}
 			if (out.size() >= 3072) {
-				auto output = fmt::to_string(out);
-				connection->SendEmoteMessageRaw(to, 0, AccountStatus::Player, CC_NPCQuestSay, output.c_str());
+				connection->SendEmoteMessageRaw(to, 0, AccountStatus::Player, CC_NPCQuestSay, out.data());
 				addnewline = false;
 				out.clear();
 			}
