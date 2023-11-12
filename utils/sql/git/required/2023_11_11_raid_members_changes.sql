497eb05320150d33be70f80311decd9bd140088f
Lead Generalist MTF Engineer <espause0703@gmail.com>
2023-11-11 21:33:31 -0500

    Update 2023_11_11_raid_members_changes.sql

--- utils/sql/git/required/2023_11_11_raid_members_changes.sql
+++ utils/sql/git/required/2023_11_11_raid_members_changes.sql
@@ -1,7 +1,3 @@
-ALTER TABLE `character_data` 
-ADD COLUMN `e_self_found` tinyint(4) NOT NULL DEFAULT 0 AFTER `showhelm`,
-ADD COLUMN `e_solo_only` tinyint(4) NOT NULL DEFAULT 0 AFTER `e_self_found`,
-ADD COLUMN `e_hardcore` tinyint(4) NOT NULL DEFAULT 0 AFTER `e_solo_only`;
-
-ALTER TABLE  `character_data` 
-ADD COLUMN `e_hardcore_death_time` bigint(22) NOT NULL DEFAULT 0 AFTER `e_hardcore`;
+ALTER TABLE `raid_members` 
+ADD COLUMN `guildid` int(11) UNSIGNED NOT NULL DEFAULT 0 AFTER `islooter`,
+ADD COLUMN `is_officer` tinyint(1) UNSIGNED NOT NULL DEFAULT 0 AFTER `guildid`;
