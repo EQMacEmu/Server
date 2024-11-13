bbd8993065a96e6db96dd9d5099d445d5bdc7e27
regneq <rregenn@gmail.com>
2024-11-13 15:52:56 -0800

    revert player_damaged change.

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -1773,7 +1773,7 @@ bool NPC::Death(Mob* killerMob, int32 damage, uint16 spell, EQ::skills::SkillTyp
 	Mob *oos = nullptr;
 	bool skip_corpse_checks = false;
 	bool ismerchant = class_ == Class::Merchant || MerchantType > 0;
-	bool player_damaged = ds_damage + npc_damage < player_damage;
+	bool player_damaged = ds_damage + npc_damage < total_damage;
 	bool corpse = false;
 	bool xp = false;
 	bool faction = false;
